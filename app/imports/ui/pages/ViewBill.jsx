49a81da53a0336d7979ca576e05ebfd37fd8b10a
Ujjwal <ugautam@hawaii.edu>
2022-09-02 23:17:39 -1000

    ViewBill page WIP

--- app/imports/ui/pages/ViewBill.jsx
+++ app/imports/ui/pages/ViewBill.jsx
@@ -12,35 +12,68 @@ export default ViewBill = () => {
   return (
     <Container className="viewbill-container">
       <Row>
-        <Col>
-          <h2>Bill | Resolution Tracking</h2>
-          <Breadcrumb>
-            <Breadcrumb.Item href="#">Current Bill</Breadcrumb.Item>
-            <Breadcrumb.Item href="#">Measure(Status)</Breadcrumb.Item>
-          </Breadcrumb>
-        </Col>
+        <center>
+          <Col>
+            <h2>Bill | Resolution Tracking</h2>
+            <Breadcrumb className="viewbill-breadcrumb">
+              <Breadcrumb.Item href="#">Current Bill</Breadcrumb.Item>
+              <Breadcrumb.Item href="#">Measure(Status)</Breadcrumb.Item>
+            </Breadcrumb>
+          </Col>
+        </center>
       </Row>
-      <Row>
+      <Row className="viewbill-row">
         <Col>
           {/* <Card style={{ width: "18rem" }} className="text-center"> */}
-          <ListGroup variant="flush">
+          <ListGroup>
             <ListGroup.Item as="li" className="d-flex">
-              <div className="ms-2 me-auto listview-div">
+              <div className="ms-2 me-auto viewbill-div">
                 <span className="fw-bold">Bill/Resolution No</span>
-                <span>Cras justo odio</span>
+                <span>AA1, BA1, CA2, DVB3</span>
               </div>
             </ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            <ListGroup.Item as="li" className="d-flex">
+              <div className="ms-2 me-auto viewbill-div">
+                <span className="fw-bold">Office</span>
+                <span>OSIP, OPS, OCID, ABCD, EFGH, HIJK</span>
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item as="li" className="d-flex">
+              <div className="ms-2 me-auto viewbill-div">
+                <span className="fw-bold">Action</span>
+                <span>Testimony</span>
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item as="li" className="d-flex">
+              <div className="ms-2 me-auto viewbill-div">
+                <span className="fw-bold">Status</span>
+                <span>2nd Crossover</span>
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item as="li" className="d-flex">
+              <div className="ms-2 me-auto viewbill-div">
+                <span className="fw-bold">Companion</span>
+                <span>What goes here?</span>
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item as="li" className="d-flex">
+              <div className="ms-2 me-auto viewbill-div">
+                <span className="fw-bold">Legislation Type</span>
+                <span>Bill</span>
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item as="li" className="d-flex">
+              <div className="ms-2 me-auto viewbill-div">
+                <span className="fw-bold">Committee Referral</span>
+                <span>ABC, DEF, GHI</span>
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item as="li" className="d-flex">
+              <div className="ms-2 me-auto viewbill-div">
+                <span className="fw-bold">Introduced By</span>
+                <span>ABC, DEF, GHI</span>
+              </div>
+            </ListGroup.Item>
           </ListGroup>
           {/* </Card> */}
         </Col>
