fb6237b90f5a9189f9072522cb2c586a919a5e8e
Ujjwal <ugautam@hawaii.edu>
2022-09-03 20:16:10 -1000

    ViewBill page WIP

--- app/imports/ui/pages/ViewBill.jsx
+++ app/imports/ui/pages/ViewBill.jsx
@@ -218,11 +218,6 @@ export default ViewBill = () => {
           {/* </Card> */}
         </Col>
       </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
     </Container>
   );
 };
