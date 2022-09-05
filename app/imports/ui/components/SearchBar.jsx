6c04149b6daa605e0b1ff0c959cf99d2142b1820
charlie-robison <charlierobison480@gmail.com>
2022-09-04 19:41:49 -1000

    Background now becomes transparent once search button is clicked. Foreground search bar UI will be added on top of this.
    Created a useState hook to hold the state of the search button click and also created a useState to hold the state of the style for the App.jsx. The setStyle function is now passed as a prop in the SearchBar component to be used to set the style for App.jsx when the button click state changes.

--- app/imports/ui/components/SearchBar.jsx
+++ app/imports/ui/components/SearchBar.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => (
-  <Container>
-    <Row>
-      <Col xs={11} />
-      <Col>
-        <Button variant="outline-dark">
-          <BsSearch />
-        </Button>
-      </Col>
-    </Row>
-  </Container>
-);
+const SearchBar = ({ setStyle }) => {
+  const style1 = { opacity: '1' };
+  const style2 = { opacity: '0.2' };
+  const [click, setClick] = useState(false);
+
+  useEffect(() => {
+    if (click) {
+      setStyle(style2);
+    } else {
+      setStyle(style1);
+    }
+  });
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={11} />
+        <Col>
+          <Button variant="outline-dark" onClick={() => setClick(!click)}>
+            <BsSearch />
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+SearchBar.propTypes = {
+  setStyle: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
@@ -20,29 +20,35 @@ import ViewBills from '../pages/ViewBills';
 import SearchBar from '../components/SearchBar';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
-const App = () => (
-  <Router>
-    <div className="d-flex flex-column min-vh-100">
-      <NavBar />
-      <SearchBar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/bills" element={<ViewBills />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signout" element={<SignOut />} />
-        <Route path="/home" element={<ProtectedRoute><Landing /></ProtectedRoute>} />
-        <Route path="/list" element={<ProtectedRoute><ListStuff /></ProtectedRoute>} />
-        <Route path="/add" element={<ProtectedRoute><AddStuff /></ProtectedRoute>} />
-        <Route path="/edit/:_id" element={<ProtectedRoute><EditStuff /></ProtectedRoute>} />
-        <Route path="/admin" element={<AdminProtectedRoute><ListStuffAdmin /></AdminProtectedRoute>} />
-        <Route path="/notauthorized" element={<NotAuthorized />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
-  </Router>
-);
+const App = () => {
+  const style1 = { backgroundColor: 'white' };
+  const [style, setStyle] = useState(style1);
+  return (
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <NavBar />
+        <SearchBar setStyle={setStyle} />
+        <div style={style}>
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/bills" element={<ViewBills />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signout" element={<SignOut />} />
+            <Route path="/home" element={<ProtectedRoute><Landing /></ProtectedRoute>} />
+            <Route path="/list" element={<ProtectedRoute><ListStuff /></ProtectedRoute>} />
+            <Route path="/add" element={<ProtectedRoute><AddStuff /></ProtectedRoute>} />
+            <Route path="/edit/:_id" element={<ProtectedRoute><EditStuff /></ProtectedRoute>} />
+            <Route path="/admin" element={<AdminProtectedRoute><ListStuffAdmin /></AdminProtectedRoute>} />
+            <Route path="/notauthorized" element={<NotAuthorized />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div>
+    </Router>
+  );
+};
 
 /*
  * ProtectedRoute (see React Router v6 sample)
