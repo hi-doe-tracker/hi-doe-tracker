f371d97354882ca71ebac3e0d2fdd079dbf16685
charlie-robison <charlierobison480@gmail.com>
2022-09-06 11:17:06 -1000

    Added link to each suggestion which would allow for future linking to a view bill page corresponding to the suggestion given.

--- app/imports/ui/components/SearchSuggestions.jsx
+++ app/imports/ui/components/SearchSuggestions.jsx
@@ -4,7 +4,21 @@ import { ListGroup, Row } from 'react-bootstrap';
 
 const SearchSuggestions = ({ searchWord }) => {
   const searchSuggestionsStyle = { position: 'absolute', top: '237px', right: '200px', width: '972px', height: '10px' };
-  const testSearchSuggestions = [`Suggestion 1 for ${searchWord}`, `Suggestion 2 for ${searchWord}`, `Suggestion 3 for ${searchWord}`];
+  const linkStyle = { color: 'black', textDecoration: 'none' };
+  const testSearchSuggestions = [
+    {
+      name: `Suggestion 1 for ${searchWord}`,
+      link: 'Suggestion 1 Link',
+    },
+    {
+      name: `Suggestion 2 for ${searchWord}`,
+      link: 'Suggestion 2 Link',
+    },
+    {
+      name: `Suggestion 3 for ${searchWord}`,
+      link: 'Suggestion 3 Link',
+    },
+  ];
 
   // Displays no suggestions is searchWord is nothing.
   if (searchWord === '') {
@@ -15,7 +29,7 @@ const SearchSuggestions = ({ searchWord }) => {
   return (
     <Row style={searchSuggestionsStyle}>
       <ListGroup>
-        {testSearchSuggestions.map((suggestion) => <ListGroup.Item key={suggestion}>{suggestion}</ListGroup.Item>)}
+        {testSearchSuggestions.map((suggestion) => <ListGroup.Item key={suggestion.name}><a href={suggestion.link} style={linkStyle}>{suggestion.name}</a></ListGroup.Item>)}
       </ListGroup>
     </Row>
   );
