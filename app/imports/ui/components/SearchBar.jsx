a79080d68dbadb57c8c06636812af8f37f6daec3
charlie-robison <charlierobison480@gmail.com>
2022-09-12 11:12:32 -1000

    Added mobile adjusting for Search Bar.

--- app/imports/ui/components/SearchBar.jsx
+++ app/imports/ui/components/SearchBar.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import { useMediaQuery } from 'usehooks-ts';
 import { Col, Form, InputGroup, Row } from 'react-bootstrap';
 import { BsSearch } from 'react-icons/bs';
 import PropTypes from 'prop-types';
 
 const SearchBar = ({ setSearchWord }) => {
-  // const searchBarStyle = { margin: 'auto', top: '100px', right: '-100px', width: '100%' };
+  const mobileView = useMediaQuery('(max-width: 760px)');
+  const searchBarStyle = { margin: 'auto', top: '100px', right: '-100px', width: '100%' };
   const searchBarStyleMobile = { margin: 'auto', top: '100px', right: '-50px', width: '80%' };
+  // Holds the state for the style based on screen dimensions.
   // Sets searchWord to the inputted search word.
   const checkSearchWord = event => {
     setSearchWord(event.target.value);
   };
+
+  // Modifies search bar UI based on screen dimensions.
+  if (mobileView) {
+    return (
+      <Row>
+        <Col xs={10}>
+          <InputGroup style={searchBarStyleMobile} onChange={checkSearchWord}>
+            <InputGroup.Text><BsSearch /></InputGroup.Text>
+            <Form.Control id="inlineFormInputGroup" placeholder="Search" />
+          </InputGroup>
+        </Col>
+      </Row>
+    );
+  }
   return (
     <Row>
       <Col xs={10}>
-        <InputGroup style={searchBarStyleMobile} onChange={checkSearchWord}>
+        <InputGroup style={searchBarStyle} onChange={checkSearchWord}>
           <InputGroup.Text><BsSearch /></InputGroup.Text>
           <Form.Control id="inlineFormInputGroup" placeholder="Search" />
         </InputGroup>
