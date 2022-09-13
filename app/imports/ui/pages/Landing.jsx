369cae181afce591bbaeb69cdd04c09df01237a1
Gaila Galano <gailag@hawaii.edu>
2022-09-12 14:02:52 -1000

    updated progress

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,19 +4,21 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id={PAGE_IDS.LANDING} className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
+  <div className="landing-background">
+    <Container id={PAGE_IDS.LANDING} className="py-3">
+      <Row className="align-middle text-center">
+        <Col xs={4}>
+          <Image roundedCircle src="/images/state-logo.png" width="250px" />
+        </Col>
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Col>
+        <Col xs={8} className="d-flex flex-column justify-content-center">
+          <h1>Hawaii State Legislature</h1>
+          <p>Bill & Testimony Database</p>
+        </Col>
 
-    </Row>
-  </Container>
+      </Row>
+    </Container>
+  </div>
 );
 
 export default Landing;
