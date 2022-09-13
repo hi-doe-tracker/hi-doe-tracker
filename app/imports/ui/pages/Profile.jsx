ed2783bbeb699ebf8f98ecf549515f3174c69853
kerensaa <kerensaa@stud.ntnu.no>
2022-09-12 21:39:11 -1000

    Add profile photo button and fix ESLint errors

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
-import { PAGE_IDS } from '../utilities/PageIDs';
 import '/client/style.css';
-import { Card, Col, Container, Row } from 'react-bootstrap';
-
+import { Card, Container } from 'react-bootstrap';
+import { PAGE_IDS } from '../utilities/PageIDs';
 
 const Profile = () => {
   const user = {
     name: 'Jane Doe',
     title: 'Administrative associate',
-    priv: 'Admin',
     email: 'Jane.doe@hidoe.com',
     image: '/images/profile-image.png',
   };
   return (
     <Container id={PAGE_IDS.PROFILE} className="py-3">
-        
-        {/* <Row>
+      {/* <Row>
             <Col xs={4}>
                 <Image src="/images/meteor-logo.png"></Image>
             </Col>
         </Row> */}
-
-        
-      <Card >
-        
-        <Card.Body className="text-center">
-        <Card.Img
+      <div className="card">
+        <div className="card-body text-center">
+          <Card.Img
             src={user.image}
             className="rounded-circle img-thumbnail "
             style={{ width: '20rem' }}
           />
-          <Card.Title className="profile-card">{user.name}</Card.Title>
-          <Card.Text>{user.priv}</Card.Text>
-          <Card.Text>{user.title}</Card.Text>
-          <Card.Text>{user.email}</Card.Text>
-        </Card.Body>
-      </Card>
+          <br />
+          <button type="button" className="btn btn-light mt-2">Change profile photo</button>
+
+          {/* <Card.Title className="profile-card mt-4">{user.name}</Card.Title> */}
+          <h4 className="mt-4">{user.name}</h4>
+          <p className="card-text">{user.title}</p>
+          <p className="card-text">{user.email}</p>
+        </div>
+      </div>
     </Container>
   );
 };
