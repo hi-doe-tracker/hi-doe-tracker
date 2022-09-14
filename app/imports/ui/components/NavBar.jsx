0735545a5a9e4fdbfa3a557e2e16c2f938dc4339
charlie-robison <charlierobison480@gmail.com>
2022-09-13 20:44:29 -1000

    Created Component IDs for pages on NavBar.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -26,9 +26,9 @@ const NavBar = () => {
         <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
           <Nav className="me-auto justify-content-start">
             {currentUser ? ([
-              <Nav.Link id={COMPONENT_IDS.NAVBAR_ADD_STUFF} as={NavLink} to="/add" key="add" style={menuStyle2}>Home</Nav.Link>,
-              <LinkContainer to="/bills"><Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list" style={menuStyle2}>View Bills</Nav.Link></LinkContainer>,
-              <Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} to="/list" key="list" style={menuStyle2}>Submit Testimony</Nav.Link>,
+              <Nav.Link id={COMPONENT_IDS.NAVBAR_HOME_PAGE} as={NavLink} to="/add" key="add" style={menuStyle2}>Home</Nav.Link>,
+              <LinkContainer to="/bills"><Nav.Link id={COMPONENT_IDS.NAVBAR_VIEW_BILLS_PAGE} as={NavLink} to="/list" key="list" style={menuStyle2}>View Bills</Nav.Link></LinkContainer>,
+              <Nav.Link id={COMPONENT_IDS.NAVBAR_SEND_HEARING_NOTICE_PAGE} as={NavLink} to="/list" key="list" style={menuStyle2}>Submit Testimony</Nav.Link>,
             ]) : ''}
             {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
               [<Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} to="/admin" key="admin">Admin</Nav.Link>,
