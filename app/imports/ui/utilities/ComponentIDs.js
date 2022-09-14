418446b653200cf1f6df1cdea85a379121333924
charlie-robison <charlierobison480@gmail.com>
2022-09-13 14:37:20 -1000

    Created NAVBAR_VIEW_BILLS component id.

--- app/imports/ui/utilities/ComponentIDs.js
+++ app/imports/ui/utilities/ComponentIDs.js
@@ -1,6 +1,7 @@
 export const COMPONENT_IDS = {
   LIST_STUFF_EDIT: 'list-stuff-edit',
   NAVBAR_ADD_STUFF: 'navbar-add-stuff',
+  NAVBAR_VIEW_BILLS: 'navbar-view-bills',
   NAVBAR_LIST_STUFF: 'navbar-list-stuff',
   NAVBAR_LIST_STUFF_ADMIN: 'navbar-list-stuff-admin',
   NAVBAR_CURRENT_USER: 'navbar-current-user',
