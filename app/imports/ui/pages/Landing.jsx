539386b3a7fc4b5c042c7aed073952e68c4c7517
Gaila Galano <gailag@hawaii.edu>
2022-09-14 02:00:20 -1000

    Updated progress

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,21 +4,22 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <div className="landing-background">
-    <Container id={PAGE_IDS.LANDING} className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image roundedCircle src="/images/state-logo.png" width="250px" />
-        </Col>
+  <Container id={PAGE_IDS.LANDING} className="py-3">
+    <Row className="align-middle text-center">
+      <Col xs={5}>
+        <Image roundedCircle src="/images/state-logo.png" width="250px" />
+      </Col>
 
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Hawaii State Legislature</h1>
-          <p>Bill & Testimony Database</p>
-        </Col>
+      <Col xs={6} className="d-flex flex-column justify-content-center">
+        <h1>Hawaii State Legislature</h1>
+        <p>Key portal for to get informed and involved in the legislative process.
+          The site is geared toward providing quick and easy access to the information
+          you need in order to participate in the lawmaking process.
+        </p>
+      </Col>
 
-      </Row>
-    </Container>
-  </div>
+    </Row>
+  </Container>
 );
 
 export default Landing;
