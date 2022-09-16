f47c16c6a1a6488ceae41423cb3ddee62be0e7d8
Ujjwal <ugautam@hawaii.edu>
2022-09-15 17:16:17 -1000

    creating test data for viewbill page

--- app/imports/test-utilities/viewbilltestdata.js
+++ app/imports/test-utilities/viewbilltestdata.js
@@ -2,5 +2,44 @@
 const viewBill = {
     billlink: "http://www.google.com",
     billno: "HB 012E",
-    office: ["OSIP", "OPS", "OCID", "ABCD", "EFGH", "HIJK"]
+    office: ["OSIP", "OPS", "OCID", "ABCD", "EFGH", "HIJK"],
+    action: "Testimony",
+    status: "2nd Crossover",
+    actno: 10,
+    companion: "placeholder for companion",
+    reporttitle: "Placeholder for report title",
+    legtype: "Bill",
+    committeereferral: ["com1", "com2", "com3"],
+    measuretitle: "Placeholder for measure title",
+    introducedby : "ABCDEFGHI",
+    introducedbydate: "Wed Sep 21 2022",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    allversion:["www.google.com", "www.youtube.com", "www.yahoo.com"],
+    committeereports: ["www.google.com", "www.youtube.com", "www.yahoo.com"],
+    hearingnotices: ["www.google.com", "www.youtube.com", "www.yahoo.com"],
+    laststatus: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet"],
+    notifiedhearingdate: "Wed Sep 21 2022",
+    notifiedhearing: "www.google.com",
+    hearingdate: "Wed Sep 21 2022",
+    hearingtime: "02:30 PM",
+    hearingLocation: "Cabinet Building",
+    Committee: "Senate",
+    type: "Decision Making",
+    testifiercontact: ["abc@gmail.com", "def@gmail.com"],
+    similar: "Placeholder for similar",
+    leadofficeposition: "What goes here?",
+    testifier: "Cara Tanimura",
+    approvedtestimony: ["abcdefghi.pdf", "abcdefghi.pdf", "abcdefghi.pdf"],
+    monitoringreports: "what goes here?",
+    hearingcomments: ["Comment 1", "Comment 2", "Comment 3"],
+    testimony: ["orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet", "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet"],
+    rationale: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    statustext:["orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet", "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet"],
+
+
+
+
+
+
+
 }
