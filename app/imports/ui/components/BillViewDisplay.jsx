8c2be7530dfbbaa4f184fb7fac84eee4fa435e33
Ujjwal <ugautam@hawaii.edu>
2022-09-18 14:51:37 -1000

    Added links to Bill Name and disabled delete button redirection

--- app/imports/ui/components/BillViewDisplay.jsx
+++ app/imports/ui/components/BillViewDisplay.jsx
@@ -2,36 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ListGroup, Row, Col, ProgressBar, Button } from 'react-bootstrap';
 import { useMediaQuery } from 'usehooks-ts';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 const BillViewDisplay = ({ billData }) => {
   const mobileView = useMediaQuery('(max-width: 809px)');
 
   if (mobileView) {
     return (
-      <LinkContainer to="/viewbill">
-        <ListGroup.Item action href={billData.url}>
-          <Row><Col>{billData.billName}</Col></Row>
-          <Row><Col>{billData.date}</Col></Row>
-          <Row><Col>{billData.offices.toString()}</Col></Row>
-          <Row><Col>Progress<ProgressBar now={billData.progress} /><br /></Col></Row>
-          <Row><Col><Button variant="outline-danger">Delete</Button></Col></Row>
-        </ListGroup.Item>
-      </LinkContainer>
+      <ListGroup.Item action>
+        <Row><Col><Link id="bill-view" to="/viewbill">{billData.billName}</Link></Col></Row>
+        <Row><Col>{billData.date}</Col></Row>
+        <Row><Col>{billData.offices.toString()}</Col></Row>
+        <Row><Col>Progress<ProgressBar now={billData.progress} /><br /></Col></Row>
+        <Row><Col><Button variant="outline-danger" onClick={() => window.alert('Button Clicked')}>Delete</Button></Col></Row>
+      </ListGroup.Item>
     );
   }
   return (
-    <LinkContainer id="bill-view" to="/viewbill">
-      <ListGroup.Item action href={billData.url}>
-        <Row>
-          <Col sm="2">{billData.billName}</Col>
-          <Col sm="2">{billData.date}</Col>
-          <Col sm="3">{billData.offices.toString()}</Col>
-          <Col sm="3">Progress<ProgressBar now={billData.progress} /></Col>
-          <Col sm="1"><Button variant="outline-danger">Delete</Button></Col>
-        </Row>
-      </ListGroup.Item>
-    </LinkContainer>
+    <ListGroup.Item action>
+      <Row>
+        <Col sm="2"><Link id="bill-view" to="/viewbill">{billData.billName}</Link></Col>
+        <Col sm="2">{billData.date}</Col>
+        <Col sm="3">{billData.offices.toString()}</Col>
+        <Col sm="3">Progress<ProgressBar now={billData.progress} /></Col>
+        <Col sm="1"><Button variant="outline-danger" onClick={() => window.alert('Button Clicked')}>Delete</Button></Col>
+      </Row>
+    </ListGroup.Item>
   );
 };
 
