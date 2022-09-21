55717268e5c8993788d8298b0da9db4c63be824c
Gaila Galano <gailag@hawaii.edu>
2022-09-20 19:28:46 -1000

    updated progress

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,7 +3,6 @@ import { Col, Container, Image, Row, Button } from 'react-bootstrap';
 import { useMediaQuery } from 'usehooks-ts';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
-/* A simple static component to render some text for the landing page. */
 const Landing = () => {
   const mobileView = useMediaQuery('(max-width: 760px)');
 
@@ -13,7 +12,9 @@ const Landing = () => {
         <Container id={PAGE_IDS.LANDING} className="py-3">
           <Row className="align-middle text-center">
             <Col xs={5}>
-              <Image roundedCircle src="/images/state-logo.png" width="300px" />
+              <div className="state-logo-pic">
+                <Image src="/images/state-logo.png" class="image-fluid" />
+              </div>
             </Col>
 
             <Col xs={6} className="d-flex flex-column justify-content-center">
