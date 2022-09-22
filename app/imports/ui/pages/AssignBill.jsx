cdb327603e0d8d8c604d7c213715b5421a66aa68
charlie-robison <charlierobison480@gmail.com>
2022-09-21 22:16:38 -1000

    Created option to pick existing bills and select what offices they are assigned to.

--- app/imports/ui/pages/AssignBill.jsx
+++ app/imports/ui/pages/AssignBill.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
+import { PAGE_IDS } from '../utilities/PageIDs';
+
+const bills = [
+  {
+    billId: 1,
+    billName: 'First Bill',
+  },
+  {
+    billId: 2,
+    billName: 'Second Bill',
+  },
+  {
+    billId: 3,
+    billName: 'Third Bill',
+  },
+];
+
+const offices = ['DEPUTY', 'OCID', 'OFO', 'OFS', 'OITS', 'OSIP', 'OSSS', 'OTM'];
 
 const AssignBill = () => (
-  <Container>
-    Assign Bill Page
+  <Container id={PAGE_IDS.ASSIGN_BILLS}>
+    <Form>
+      <h3>Assigned Bill</h3>
+      <Form.Select>
+        <option>Assign an existing bill</option>
+        {bills.map(bill => <option key={bill.id} value={bill.id}>{bill.billName}</option>)}
+      </Form.Select>
+      <h3>Offices</h3>
+      {offices.map(office => (
+        <Form.Check
+          inline
+          label={office}
+          name={office}
+          type="checkbox"
+          id="inline-checkbox-1"
+        />
+      ))}
+    </Form>
   </Container>
 );
 
