d9106e07182f43c6fdccbfa96a8ec02d98f56049
charlie-robison <charlierobison480@gmail.com>
2022-09-21 21:12:46 -1000

    Created the AssignBill page.

---
+++ app/imports/ui/pages/AssignBill.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const AssignBill = () => (
+  <Container>
+    Assign Bill Page
+  </Container>
+);
+
+export default AssignBill;
