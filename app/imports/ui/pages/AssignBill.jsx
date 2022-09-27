d92d7d4e82178fb001005aab72f68ad7e7218af1
charlie-robison <charlierobison480@gmail.com>
2022-09-26 14:58:46 -1000

    Created UI for all input fields.

--- app/imports/ui/pages/AssignBill.jsx
+++ app/imports/ui/pages/AssignBill.jsx
@@ -1,95 +1,54 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Card, Col, Row } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import swal from 'sweetalert';
+// import swal from 'sweetalert';
+import { AutoForm, ListAddField, RadioField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { ScraperBills } from '../../api/scraperbill/ScraperBillCollection';
 import LoadingSpinner from '../components/LoadingSpinner';
-import { Stuffs } from '../../api/stuff/StuffCollection';
-import { defineMethod } from '../../api/base/BaseCollection.methods';
-
-const officeNames = ['DEPUTY', 'OCID', 'OFO', 'OFS', 'OITS', 'OSIP', 'OSSS', 'OTM'];
+// import { defineMethod } from '../../api/base/BaseCollection.methods';
 
 // Creates a schema based on if the data is ready and the scraper bills given.
-const createSchema = (ready, scraperBills) => {
-  if (ready) {
-    // Create a schema to specify the structure of the data to appear in the form.
-    return new SimpleSchema({
-      assignedBill: {
-        type: String,
-        allowedValues: scraperBills,
-        defaultValue: 'Assign an existing bill',
-      },
-      offices: {
-        type: String,
-        allowedValues: ['DEPUTY', 'OCID', 'OFO', 'OFS', 'OITS', 'OSIP', 'OSSS', 'OTM'],
-        defaultValues: 'Pick an Office',
-      },
-      action: String,
-      actionnumber: String,
-      legaltype: String,
-      committeereferral: String,
-      allversions: String,
-      committeereports: String,
-      hearingnotices: String,
-      notifiedhearingdate: String,
-      notifiedhearing: String,
-      hearingdate: String,
-      hearingtime: String,
-      hearinglocation: String,
-      committee: String,
-      type: String,
-      testifiercontact: String,
-      similar: String,
-      leadofficeposition: String,
-      testifier: String,
-      approvedtestimony: String,
-      monitoringreports: String,
-      hearingComments: String,
-      testimony: String,
-      rationale: String,
-    });
-  }
-
-  return new SimpleSchema({
-    assignedBill: {
-      type: String,
-      allowedValues: [],
-      defaultValue: 'Assign an existing bill',
-    },
-    offices: {
-      type: String,
-      allowedValues: ['DEPUTY', 'OCID', 'OFO', 'OFS', 'OITS', 'OSIP', 'OSSS', 'OTM'],
-      defaultValues: 'Pick an Office',
-    },
-    action: String,
-    actionnumber: String,
-    legaltype: String,
-    committeereferral: String,
-    allversions: String,
-    committeereports: String,
-    hearingnotices: String,
-    notifiedhearingdate: String,
-    notifiedhearing: String,
-    hearingdate: String,
-    hearingtime: String,
-    hearinglocation: String,
-    committee: String,
+const formSchema = new SimpleSchema({
+  'assigned bill': {
     type: String,
-    testifiercontact: String,
-    similar: String,
-    leadofficeposition: String,
-    testifier: String,
-    approvedtestimony: String,
-    monitoringreports: String,
-    hearingComments: String,
-    testimony: String,
-    rationale: String,
-  });
-};
+    allowedValues: ['1', '2', '3'],
+    defaultValue: '1',
+  },
+  offices: {
+    type: String,
+    allowedValues: ['DEPUTY', 'OCID', 'OFO', 'OFS', 'OITS', 'OSIP', 'OSSS', 'OTM'],
+    defaultValue: 'DEPUTY',
+  },
+  action: String,
+  'action number': String,
+  'legal type': String,
+  'committee referral': String,
+  'all versions': String,
+  'committee reports': String,
+  'hearing notices': String,
+  'notified hearing date': String,
+  'notified hearing': String,
+  'hearing date': String,
+  'hearing time': String,
+  'hearing location': String,
+  committee: String,
+  type: String,
+  'testifier contact': String,
+  similar: String,
+  'lead office position': String,
+  testifier: String,
+  'approved testimony': String,
+  'monitoring reports': String,
+  'hearing comments': String,
+  testimony: String,
+  rationale: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
 
 const AssignBill = () => {
   const { ready, scraperBills } = useTracker(() => {
@@ -104,13 +63,12 @@ const AssignBill = () => {
     };
   }, []);
 
-  const bridge = new SimpleSchema2Bridge(createSchema(ready, scraperBills));
-
   // On submit, insert the data.
   const submit = (data, formRef) => {
+    /*
     if (ready) {
       const {
-        assignedBill,
+        assignedbill,
         offices,
         action,
         actionnumber,
@@ -132,14 +90,14 @@ const AssignBill = () => {
         testifier,
         approvedtestimony,
         monitoringreports,
-        hearingComments,
+        hearingcomments,
         testimony,
         rationale,
       } = data;
       const owner = Meteor.user().username;
       const collectionName = Stuffs.getCollectionName();
       const definitionData = {
-        assignedBill,
+        assignedbill,
         offices,
         action,
         actionnumber,
@@ -161,10 +119,9 @@ const AssignBill = () => {
         testifier,
         approvedtestimony,
         monitoringreports,
-        hearingComments,
+        hearingcomments,
         testimony,
         rationale,
-        owner,
       };
       defineMethod.callPromise({ collectionName, definitionData })
         .catch(error => swal('Error', error.message, 'error'))
@@ -172,52 +129,82 @@ const AssignBill = () => {
           swal('Success', 'Bill added successfully', 'success');
           formRef.reset();
         });
-    }
+    } */
   };
+
+  let fRef = null;
 
   return (ready ? (
     <Container id={PAGE_IDS.ASSIGN_BILLS}>
-      <Form>
-        <h3>Assigned Bill</h3>
-        <Form.Select>
-          <option>Assign an existing bill</option>
-          {scraperBills.map(scraperBill => <option key={scraperBill._id} value={scraperBill._id}>{scraperBill.measureTitle}</option>)}
-        </Form.Select>
-        <h3>Offices</h3>
-        {officeNames.map(office => (
-          <Form.Check
-            inline
-            label={office}
-            name={office}
-            type="checkbox"
-            id="inline-checkbox-1"
-          />
-        ))}
-        <Form.Text name="Action" placeholder="Action" />
-        <Form.Text name="Action Number" placeholder="Action Number" />
-        <Form.Text name="All Versions" placeholder="All Versions" />
-        <Form.Text name="Committee Reports" placeholder="Committee Reports" />
-        <Form.Text name="Hearing Notices" placeholder="Hearing Notices" />
-        <Form.Text name="Notified Hearing Date" placeholder="Notified Hearing Date" />
-        <Form.Text name="Notified Hearing" placeholder="Notified Hearing" />
-        <Form.Text name="Hearing Date" placeholder="Hearing Date" />
-        <Form.Text name="Hearing Time" placeholder="Hearing Time" />
-        <Form.Text name="Hearing Location" placeholder="Hearing Location" />
-        <Form.Text name="Committee" placeholder="Committee" />
-        <Form.Text name="Type" placeholder="Type" />
-        <Form.Text name="Testifier Contact" placeholder="Testifier Contact" />
-        <Form.Text name="Similar" placeholder="Similar" />
-        <Form.Text name="Lead Office Position" placeholder="Lead Office Position" />
-        <Form.Text name="Testifier" placeholder="Testifier" />
-        <Form.Text name="Approved Testimony" placeholder="Approved Testimony" />
-        <Form.Text name="Monitoring Reports" placeholder="Monitoring Reports" />
-        <Form.Text name="Hearing Comments" placeholder="Hearing Comments" />
-        <Form.Text name="Testimony" placeholder="Testimony" />
-        <Form.Text name="Rationale" placeholder="Rationale" />
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <Row className="justify-content-center">
+        <Col>
+          <Col className="text-center"><h2>Assign Bill</h2></Col>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+            <Card>
+              <Card.Body>
+                {/*
+                 <h3>Assigned Bill</h3>
+                 <Form.Select>
+                   <option>Assign an existing bill</option>
+                   {scraperBills.map(scraperBill => <option key={scraperBill._id} value={scraperBill._id}>{scraperBill.measureTitle}</option>)}
+                 </Form.Select>
+                 <h3>Offices</h3>
+                 {officeNames.map(office => (
+                   <Form.Check
+                     inline
+                     label={office}
+                     name={office}
+                     type="checkbox"
+                     id="inline-checkbox-1"
+                   />
+                 ))} */}
+                <Row>
+                  <Col><SelectField name="assigned bill" /></Col>
+                  <Col><SelectField name="offices" checkboxes="true" inline="true" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="action" /></Col>
+                  <Col><TextField name="action number" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="all versions" /></Col>
+                  <Col><TextField name="committee reports" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="hearing notices" /></Col>
+                  <Col><TextField name="notified hearing date" /></Col>
+                  <Col><TextField name="notified hearing" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="hearing date" /></Col>
+                  <Col><TextField name="hearing time" /></Col>
+                  <Col><TextField name="hearing location" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="committee" /></Col>
+                  <Col><TextField name="type" /></Col>
+                  <Col><TextField name="testifier contact" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="similar" /></Col>
+                  <Col><TextField name="lead office position" /></Col>
+                  <Col><TextField name="testifier" /></Col>
+                  <Col><TextField name="approved testimony" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="monitoring reports" /></Col>
+                  <Col><TextField name="hearing comments" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="testimony" /></Col>
+                  <Col><TextField name="rationale" /></Col>
+                </Row>
+                <SubmitField value="Submit" />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
     </Container>
   ) : <LoadingSpinner message="Loading Data" />);
 };
