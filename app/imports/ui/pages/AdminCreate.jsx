cc7998abcf936138d93429ecc1ffa316d253a2e5
Tanner Young <tannerky@hawaii.edu>
2022-10-11 14:24:29 -1000

    Update AdminCreate.jsx

--- app/imports/ui/pages/AdminCreate.jsx
+++ app/imports/ui/pages/AdminCreate.jsx
@@ -4,6 +4,7 @@ import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, ErrorsField, SubmitField, TextField, SelectField } from 'uniforms-bootstrap5';
+import swal from 'sweetalert';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 import { UserProfiles } from '../../api/user/UserProfileCollection';
@@ -23,22 +24,26 @@ const SignUp = () => {
     password: String,
     position: {
       type: String,
-      allowedValues: ['Secretary', 'Writer', 'Office Approver', 'PIPE Approver', 'Final Approver', 'Processor', 'Admin']
+      allowedValues: ['Secretary', 'Writer', 'Office Approver', 'PIPE Approver', 'Final Approver', 'Processor', 'Admin'],
     },
     office: {
       type: String,
-      allowedValues: ['DEPUTY', 'OCID', 'OFO', 'OFS', 'OITS', 'OSIP', 'OSSS', 'OTM', 'Admin']
+      allowedValues: ['DEPUTY', 'OCID', 'OFO', 'OFS', 'OITS', 'OSIP', 'OSSS', 'OTM', 'Admin'],
     },
   });
   const bridge = new SimpleSchema2Bridge(schema);
 
   /* Handle SignUp submission. Create user account and a profile entry. */
-  const submit = (doc) => {
+  const submit = (doc, formRef) => {
     const collectionName = UserProfiles.getCollectionName();
     const definitionData = doc;
     // create the new UserProfile
     defineMethod.callPromise({ collectionName, definitionData })
-      .catch((err) => setError(err.reason));
+      .catch((err) => setError(err.reason))
+      .then(() => {
+        swal('Success', 'Account successfully created', 'success');
+        formRef.reset();
+      });
     return <Navigate to="/admin/createaccount" />;
   };
 
@@ -47,6 +52,7 @@ const SignUp = () => {
   // if (redirectToReferer) {
   //   return <Navigate to="/add" />;
   // }
+  let fRef = null;
   return (
     <Container id={PAGE_IDS.SIGN_UP} className="py-3">
       <Row className="justify-content-center">
@@ -54,15 +60,15 @@ const SignUp = () => {
           <Col className="text-center">
             <h2>Create an account</h2>
           </Col>
-          <AutoForm schema={bridge} onSubmit={data => submit(data)}>
+          <AutoForm schema={bridge} ref={ref => { fRef = ref; }} onSubmit={data => submit(data, fRef)}>
             <Card>
               <Card.Body>
                 <TextField id={COMPONENT_IDS.SIGN_UP_FORM_FIRST_NAME} name="firstName" placeholder="First name" />
                 <TextField id={COMPONENT_IDS.SIGN_UP_FORM_LAST_NAME} name="lastName" placeholder="Last name" />
                 <TextField id={COMPONENT_IDS.SIGN_UP_FORM_EMAIL} name="email" placeholder="E-mail address" />
                 <TextField id={COMPONENT_IDS.SIGN_UP_FORM_PASSWORD} name="password" placeholder="Password" type="password" />
-                <SelectField id={COMPONENT_IDS.SIGN_UP_FORM_POSITION} name="position" placeholder="Choose Position"/>
-                <SelectField id={COMPONENT_IDS.SIGN_UP_FORM_OFFICE} name="office" placeholder="Choose Office"/>
+                <SelectField id={COMPONENT_IDS.SIGN_UP_FORM_POSITION} name="position" placeholder="Choose Position" />
+                <SelectField id={COMPONENT_IDS.SIGN_UP_FORM_OFFICE} name="office" placeholder="Choose Office" />
                 <ErrorsField />
                 <SubmitField id={COMPONENT_IDS.SIGN_UP_FORM_SUBMIT} />
               </Card.Body>
