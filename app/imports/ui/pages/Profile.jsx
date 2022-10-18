fabc13cbec0740d05253fe0e00a70e6172a05dc7
Ujjwal <ugautam@hawaii.edu>
2022-10-17 21:18:33 -1000

    added change password component

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import '/client/style.css';
 import { Button, Card, Container, Form, Modal, ModalBody, ModalDialog, ModalFooter, ModalHeader, ModalTitle } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import swal from 'sweetalert';
 
 const Profile = () => {
   const user = {
@@ -17,10 +19,16 @@ const Profile = () => {
   });
 
   const [modalShow, setModalShow] = React.useState(false);
+  const [modalShowPassword, setModalShowPassword] = React.useState(false);
   const toggleShow = () => setModalShow(!modalShow);
+  const toggleShowPassword = () => setModalShowPassword(!modalShowPassword);
 
   const handleClose = () => {
     setModalShow(false);
+  };
+
+  const handleClosePassword = () => {
+    setModalShowPassword(false);
   };
 
   // TODO: fix non updating values
@@ -31,6 +39,23 @@ const Profile = () => {
     // }));
     handleClose();
   };
+    // TODO: fix non updating values
+    const handleSubmitPassword = (evt) => {
+      evt.preventDefault();
+      const name = Meteor.user().username;
+      const newPassword = evt.target[0].value;
+      const confirmPassword = evt.target[1].value;
+      if (name){
+        if (newPassword === confirmPassword){
+          console.log("equal")
+        }else{
+          swal("Invalid Password", "Password Do Not Match!!", "error");
+        }
+      }else{
+        swal("Invalid User", "User does not exist!!", "error");
+      }
+      handleClosePassword();
+    };
 
   return (
     <Container id={PAGE_IDS.PROFILE} className="py-3">
@@ -73,6 +98,37 @@ const Profile = () => {
                 Save Changes
               </Button>
             </ModalFooter>
+          </ModalDialog>
+        </Modal>
+      </div>
+      <div className="text-center mt-4">
+        <button type="button" onClick={toggleShowPassword} className="btn btn-light mt-2">Change Password</button>
+        <Modal show={modalShowPassword} centered>
+          <ModalDialog>
+            <ModalHeader>
+              <ModalTitle> Change Password </ModalTitle>
+              <Button className="btn-close" color="none" onClick={toggleShowPassword}> </Button>
+            </ModalHeader>
+            <ModalBody>
+              <Form onSubmit={(data) => handleSubmitPassword(data)}>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>New Password</Form.Label>
+              <Form.Control type="password" name ="newpassword" placeholder="New Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="password" name ="submitpassword" placeholder="Confirm Password" />
+             </Form.Group>
+             <Button type="submit" >
+                Save Changes
+              </Button>
+              </Form>
+            </ModalBody>
+            {/* <ModalFooter>
+              <Button type="submit" value="submit">
+                Save Changes
+              </Button>
+            </ModalFooter> */}
           </ModalDialog>
         </Modal>
       </div>
