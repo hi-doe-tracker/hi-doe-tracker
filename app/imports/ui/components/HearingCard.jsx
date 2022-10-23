8edc8c7424923ab793f43c4f7c7fddd9f7115c69
alvinedrada <aedrada@hawaii.edu>
2022-10-22 16:13:16 -1000

    Added modal to hearings

--- app/imports/ui/components/HearingCard.jsx
+++ app/imports/ui/components/HearingCard.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import { Card, Button, Badge, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Button, Badge, Row, Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const HearingCard = ({ hearing }) => (
+const HearingCard = ({ hearing }) => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+return(
   <div className="p-6">
     <Card>
       <Card.Body>
@@ -14,13 +21,26 @@ const HearingCard = ({ hearing }) => (
         </Card.Subtitle>
       </Card.Body>
       <Card.Body>
-        {hearing.description}
+        {hearing.description.substring(0,150) + "..."}
       </Card.Body>
       <Card.Footer>
         <Row>
           <div className="d-grid gap-2">
-            <Button variant="outline-secondary">Details</Button>
+            <Button variant="outline-secondary" onClick={handleShow}>Details</Button>
             <Button variant="primary">Follow</Button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>{hearing.notice}{hearing.measureType.toUpperCase()}_{hearing.measureNumber}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body><b>Hearing date:</b> {hearing.datetime}</Modal.Body>
+              <Modal.Body><b>Location:</b> {hearing.room}</Modal.Body>
+              <Modal.Body><b>Description:</b> {hearing.description}</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </div>
         </Row>
       </Card.Footer>
@@ -33,6 +53,7 @@ const HearingCard = ({ hearing }) => (
     </Card>
   </div>
 );
+};
 
 HearingCard.propTypes = {
   hearing: PropTypes.shape({
