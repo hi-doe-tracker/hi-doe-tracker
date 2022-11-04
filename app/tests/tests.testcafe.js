b3d29636294f77a981dccbc999d75ce048940ba2
tkansaki <tkansaki@hawaii.edu>
2022-11-03 22:20:20 -1000

    Fixed all current integration tests

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -1,15 +1,12 @@
-import { Selector, t } from 'testcafe';
-import { addStuffPage, listStuffAdminPage, listStuffPage, editStuffPage, manageDatabasePage } from './simple.page';
 import { signOutPage, viewBillsPage, sendHearingNoticePage, viewBillPage, assignBillPage, manageAccountsPage, viewHearingsPage, simpleSubmitTestimonyPage } from './simple.page';
 import { landingPage } from './landing.page';
 import { signInPage } from './signin.page';
 import { navBar } from './navbar.component';
 import { profilePage } from './profile.page';
 import { homePage } from './home.page';
-import { admincreatePage } from './admincreate.page';
+import { adminCreatePage } from './admincreate.page';
 import { adminManageAccountsPage } from './manageaccounts.page';
 import { submitTestimonyPage } from './submittestimony.page';
-import { COMPONENT_IDS } from '../imports/ui/utilities/ComponentIDs';
 
 /* global fixture:false, test:false */
 
@@ -25,7 +22,7 @@ test('Test that landing page shows up', async () => {
   await landingPage.isDisplayed();
 });
 
-/* test('Test that signin and signout work', async () => {
+test('Test that signin and signout work', async () => {
   await navBar.gotoSignInPage();
   await signInPage.signin(credentials.username, credentials.password);
   await navBar.isLoggedIn(credentials.username);
@@ -93,14 +90,14 @@ test('Test that admin pages show up', async () => {
   await signInPage.signin(adminCredentials.username, adminCredentials.password);
   await navBar.isLoggedIn(adminCredentials.username);
   await navBar.gotoAdminCreatePage();
-  await admincreatePage.isDisplayed();
+  await adminCreatePage.isDisplayed();
   await navBar.gotoAssignBillPage();
   await assignBillPage.isDisplayed();
   await navBar.gotoManageAccountsPage();
   await manageAccountsPage.isDisplayed();
   await navBar.logout();
   await signOutPage.isDisplayed();
-}); */
+});
 
 test('Test that submit testimony page works', async (testController) => {
   await navBar.gotoSignInPage();
@@ -112,15 +109,15 @@ test('Test that submit testimony page works', async (testController) => {
   await submitTestimonyPage.addProject(testController);
 });
 
-/* test('Test that admin create page works', async (testController) => {
-  //sign in as admin
+test('Test that admin create page works', async (testController) => {
+  // sign in as admin
   await navBar.gotoSignInPage();
   await signInPage.signin(adminCredentials.username, adminCredentials.password);
   await navBar.isLoggedIn(adminCredentials.username);
-  //create bat man user
+  // create bat man user
   await navBar.gotoAdminCreatePage();
-  await admincreatePage.signupUser(testController);
-  //log out and sign in as bat man user, then sign back out for next test
+  await adminCreatePage.signupUser(testController);
+  // log out and sign in as bat man user, then sign back out for next test
   await navBar.logout();
   await signOutPage.isDisplayed();
   await navBar.gotoSignInPage();
@@ -133,18 +130,17 @@ test('Test that admin manage accounts page works', async (testController) => {
   await navBar.gotoSignInPage();
   await signInPage.signin(adminCredentials.username, adminCredentials.password);
   await navBar.isLoggedIn(adminCredentials.username);
-  //double checks and creates the bat man user
+  // double checks and creates the bat man user
   await navBar.gotoAdminCreatePage();
-  await admincreatePage.signupUser(testController);
-  //deletes the latest entry which should be batman
+  await adminCreatePage.signupUser(testController);
+  // deletes the latest entry which should be batman
   await navBar.gotoManageAccountsPage();
   await adminManageAccountsPage.deleteUser();
-  //log out then try to log in as the deleted bat man user
+  // log out then try to log in as the deleted bat man user
   await navBar.logout();
   await signOutPage.isDisplayed();
   await navBar.gotoSignInPage();
-  //attempt to log in but it should fail, check to see if we're still on the sign in page
+  // attempt to log in but it should fail, check to see if we're still on the sign in page
   await signInPage.attemptsignin(newCredentials.username, newCredentials.password);
   await signInPage.isDisplayed();
 });
-*/
