f328d1bbbec11a451c5c47433b30f7d8b0a53011
charlie-robison <charlierobison480@gmail.com>
2022-11-28 11:10:14 -1000

    Created notification mongoDB collection to hold notifications.

---
+++ app/imports/api/notification/NotificationCollection.js
@@ -0,0 +1,102 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import { Roles } from 'meteor/alanning:roles';
+import BaseCollection from '../base/BaseCollection';
+import { ROLE } from '../role/Role';
+
+export const notificationPublications = {
+  notification: 'Notification',
+  notificationAdmin: 'NotificationAdmin',
+};
+
+class NotificationCollection extends BaseCollection {
+  constructor() {
+    super('Notifications', new SimpleSchema({
+      message: { type: String, optional: true },
+      messageType: { type: Boolean, optional: true },
+    }));
+  }
+
+  /**
+   * Defines a new Notification item.
+   * @param message the message of the notification.
+   * @param messageType the type of message.
+   * @return {String} the docID of the new document.
+   */
+  define({ message, messageType }) {
+    const docID = this._collection.insert({
+      message,
+      messageType,
+    });
+    return docID;
+  }
+
+  /**
+   * Default publication method for entities.
+   * It publishes the entire collection for admin and just the stuff associated to an owner.
+   */
+  publish() {
+    if (Meteor.isServer) {
+      const instance = this;
+      /** This subscription publishes only the documents associated with the logged in user */
+      Meteor.publish(notificationPublications.notification, function publish() {
+        if (this.userId) {
+          return instance._collection.find();
+        }
+        return this.ready();
+      });
+
+      /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+      Meteor.publish(notificationPublications.notificationAdmin, function publish() {
+        if (this.userId && Roles.userIsInRole(this.userId, ROLE.ADMIN)) {
+          return instance._collection.find();
+        }
+        return this.ready();
+      });
+    }
+  }
+
+  /**
+   * Subscription method for stuff owned by the current user.
+   */
+  subscribeNotification() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(notificationPublications.notification);
+    }
+    return null;
+  }
+
+  /**
+   * Subscription method for admin users.
+   * It subscribes to the entire collection.
+   */
+  subscribeNotificationAdmin() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(notificationPublications.notificationAdmin);
+    }
+    return null;
+  }
+
+  /**
+   * Default implementation of assertValidRoleForMethod. Asserts that userId is logged in as an Admin or User.
+   * This is used in the define, update, and removeIt Meteor methods associated with each class.
+   * @param userId The userId of the logged in user. Can be null or undefined
+   * @throws { Meteor.Error } If there is no logged in user, or the user is not an Admin or User.
+   */
+  assertValidRoleForMethod(userId) {
+    this.assertRole(userId, [ROLE.ADMIN, ROLE.USER]);
+  }
+
+  /**
+   * Returns an object representing the definition of docID in a format appropriate to the restoreOne or define function.
+   * @param docID
+   * @return {{owner: (*|number), condition: *, quantity: *, name}}
+   */
+  dumpOne(docID) {
+    const message = docID.message;
+    const messageType = docID.messageType;
+    return { message, messageType };
+  }
+}
+
+export const Notifications = new NotificationCollection();
