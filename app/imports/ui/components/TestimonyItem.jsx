df045c440d1c80183a9291911d1ea9353ba95b65
charlie-robison <charlierobison480@gmail.com>
2022-11-29 20:52:20 -1000

    Approvers of different offices can no longer approve other office testimonies.

--- app/imports/ui/components/TestimonyItem.jsx
+++ app/imports/ui/components/TestimonyItem.jsx
@@ -247,19 +247,20 @@ const TestimonyItem = ({ testimony }) => {
         {progressState === 'danger' ? <h3>Failed!</h3> : <div />}
         <form>
           <div>
-            <input type="checkbox" id="officeBox" defaultChecked={checkbox1} disabled={userProfile.position !== 'Office Approver'} onChange={() => changeCheckbox(1)} />
+            {console.log(associatedBill.mainOffice)}
+            <input type="checkbox" id="officeBox" defaultChecked={checkbox1} disabled={userProfile.position !== 'Office Approver' || userProfile.assignedOffice !== associatedBill.mainOffice} onChange={() => changeCheckbox(1)} />
             &nbsp;&nbsp;
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="officeBox">Office Approval Status</label>
           </div>
           <div>
-            <input type="checkbox" id="pipeBox" defaultChecked={checkbox2} disabled={userProfile.position !== 'PIPE Approver'} onChange={() => changeCheckbox(2)} />
+            <input type="checkbox" id="pipeBox" defaultChecked={checkbox2} disabled={userProfile.position !== 'PIPE Approver' || userProfile.assignedOffice !== associatedBill.mainOffice} onChange={() => changeCheckbox(2)} />
             &nbsp;&nbsp;
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="officeBox">PIPE Approval Status</label>
           </div>
           <div>
-            <input type="checkbox" id="finalBox" defaultChecked={checkbox3} disabled={userProfile.position !== 'Final Approver'} onChange={() => changeCheckbox(3)} />
+            <input type="checkbox" id="finalBox" defaultChecked={checkbox3} disabled={userProfile.position !== 'Final Approver' || userProfile.assignedOffice !== associatedBill.mainOffice} onChange={() => changeCheckbox(3)} />
             &nbsp;&nbsp;
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="officeBox">Final Approval Status</label>
