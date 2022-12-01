43eb42408c683ab29a1aa640c9594e967de5075f
kerensaa <kerensaa@stud.ntnu.no>
2022-11-30 17:30:01 -1000

    button updating variable #127

--- app/imports/ui/pages/ViewBills.jsx
+++ app/imports/ui/pages/ViewBills.jsx
@@ -53,7 +53,7 @@ const officeNames = [
     eventKey: 'otm-bills',
   },
 ];
-let sortedBills;
+
 /** Displays all bills that were assigned to a scraper bill by admin. */
 const ViewBills = () => {
   const { ready, userProfile, scraperBills } = useTracker(() => {
@@ -72,27 +72,61 @@ const ViewBills = () => {
   }, []);
 
   const [eventKey, setEventKey] = useState('');
+  const [name, setName] = useState('');
+  const [sort, setSort] = useState('');
+
+  // switch (sort) {
+  // case 'oldest':
+  //   sortedBills = 'oldest';
+  //   // console.log(sortedBills);
+  //   break;
+  // case 'newest':
+  //   sortedBills = 'newest';
+  //   // console.log(sortedBills);
+  //   break;
+  // default:
+  // }
+
+  function foo() {
+    console.log(sort);
+    return sort;
+  }
+
   useEffect(() => {
     const assignedOffice = ready ? userProfile.assignedOffice : 'ALL BILLS';
     const officeEventKey = officeNames.filter(office => office.name === assignedOffice)[0].eventKey;
     setEventKey(officeEventKey);
+    setName(assignedOffice);
+    // setSort('newest');
   }, []);
-  // console.log(eventKey)
-
-  const [sorting, setSorting] = useState('');
-
-  switch (sorting) {
-  case 'oldest':
-    sortedBills = 'oldest';
-    break;
-  case 'newest':
-    sortedBills = 'newest';
-    break;
-  default:
-    break;
-  }
   const mobileView = useMediaQuery('(max-width: 850px)');
   const hStyle = { marginLeft: '20px' };
+
+  // const sortedBillList = () => {
+  //   return (1 === 0) ?
+  //     'Empty'
+  //     : <BillViewTab
+  //       key={name}
+  //       eventKey={eventKey}
+  //       officeName={name}
+  //       sortedBills={foo()}
+  //     />
+  //   ;
+  // };
+
+  const sortedBillList = () => {
+    const table =
+      (
+        <BillViewTab
+          key={name}
+          eventKey={eventKey}
+          officeName={name}
+          sortedBills={foo()}
+        />
+      );
+    return table;
+  };
+
   return (ready ? (
     <Container id={PAGE_IDS.VIEW_BILLS} key="viewbills-container">
       <Tab.Container defaultActiveKey={eventKey}>
@@ -104,18 +138,17 @@ const ViewBills = () => {
             <Nav variant="pills" className={mobileView ? 'mb-3' : 'flex-column'}>
               {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (<Nav.Item><Nav.Link eventKey="unassigned-bills">UNASSIGNED BILLS</Nav.Link></Nav.Item>) : <div />}
               {officeNames.map((office) => (
-                <Nav.Item id="officeitem" key={office.name} onClick={() => (setEventKey(office.eventKey))}><Nav.Link eventKey={office.eventKey}>{office.name}</Nav.Link></Nav.Item>
+                <Nav.Item id="officeitem" key={office.name} onClick={() => { setEventKey(office.eventKey); setName(office.name); }}><Nav.Link eventKey={office.eventKey}>{office.name}</Nav.Link></Nav.Item>
               ))}
             </Nav>
             {mobileView ? <br /> : <div />}
           </Col>
           <Col sm="8">
             <DropdownButton id="dropdown-basic-button" title="Sort bills" style={{ float: 'right' }}>
-              {/* <Dropdown.Item onClick={() => setSorting('oldest')}> Hearing date (oldest)</Dropdown.Item> */}
-              <Dropdown.Item eventKey={eventKey} onClick={() => (setSorting('oldest'))}>
+              <Dropdown.Item eventKey={eventKey} onClick={() => { setSort('oldest'); }}>
                 Hearing date (oldest)
               </Dropdown.Item>
-              <Dropdown.Item eventKey={eventKey} onClick={() => setSorting('newest')}>Hearing date (newest)</Dropdown.Item>
+              <Dropdown.Item eventKey={eventKey} onClick={() => { setSort('newest'); }}> Hearing date (newest)</Dropdown.Item>
             </DropdownButton>
             <Tab.Content>
               {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
@@ -133,14 +166,17 @@ const ViewBills = () => {
                   </Table>
                 </Tab.Pane>
               ) : <div />}
-              {officeNames.map((officeName) => (
+              { sortedBillList() }
+
+              {/* {officeNames.map((officeName) => (
                 <BillViewTab
                   key={officeName.name}
                   eventKey={officeName.eventKey}
                   officeName={officeName.name}
-                  sortedBills={sortedBills}
+                  sortedBills={foo()}
                 />
-              ))}
+              ))} */}
+
             </Tab.Content>
           </Col>
         </Row>
